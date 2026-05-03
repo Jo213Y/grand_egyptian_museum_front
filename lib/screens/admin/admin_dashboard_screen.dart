b8b78782c81704cb62fab61348af91d4a38bd238
joe <yousifamir213@gmail.com>
2026-05-03 22:43:00 +0300

    final

--- lib/screens/admin/admin_dashboard_screen.dart
+++ lib/screens/admin/admin_dashboard_screen.dart
@@ -117,6 +117,16 @@ class _AdminDashboardScreenState extends State<AdminDashboardScreen>
                         ],
                       ),
                     ),
+                    Tab(
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(Icons.bookmark_outlined, size: 18),
+                          SizedBox(width: 2),
+                          Text('Tickets', style: TextStyle(fontSize: 12)),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -132,6 +142,8 @@ class _AdminDashboardScreenState extends State<AdminDashboardScreen>
                     UsersTab(),
                     HallEditTab(),
                     LogsTab(),
+                    BookingsTab(),
+
                   ],
                 ),
               ),
